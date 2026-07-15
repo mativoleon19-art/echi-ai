01d586311d591954ccef3c252b2dd17564b46c57
mativoleon19-art <mativoleon19@gmail.com>
2026-07-15 11:51:19 +0300

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDBvH796liFuh0sBLBzB4iuSp65iO7fDCs",
+  authDomain: "echo-89b77.firebaseapp.com",
+  databaseURL: "https://echo-89b77-default-rtdb.firebaseio.com",
+  projectId: "echo-89b77",
+  storageBucket: "echo-89b77.firebasestorage.app",
+  messagingSenderId: "907071357439",
+  appId: "1:907071357439:web:a41bf8e77acd567f9067ca",
+  measurementId: "G-8C2RZK9MJQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
